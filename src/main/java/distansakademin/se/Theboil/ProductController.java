d5b6e757cc900773968a9886015e85316d32d14d
Lapotyy <148191381+Lapotyy@users.noreply.github.com>
2024-06-15 11:20:48 +0200

    added insta and tiktok logo

--- src/main/java/distansakademin/se/Theboil/ProductController.java
+++ src/main/java/distansakademin/se/Theboil/ProductController.java
@@ -1,11 +1,10 @@
 package distansakademin.se.Theboil;
 
-import distansakademin.se.Theboil.Product;
-import distansakademin.se.Theboil.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -17,8 +16,36 @@ public class ProductController {
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Product> products = productRepository.findAll();
-        model.addAttribute("products", products);
+        model.addAttribute("welcomeMessage", "Välkommen till TheBoil");
+        model.addAttribute("discoverMessage", "Upptäck våra fantastiska menyer");
+        model.addAttribute("findUsMessage", "HITTA OSS NÄRA DIG");
         return "index";
     }
+
+    @GetMapping("/meny")
+    public String meny(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "meny";
+    }
+
+    @GetMapping("/om-oss")
+    public String omOss() {
+        return "om-oss";
+    }
+
+    @GetMapping("/kontakta-oss")
+    public String kontaktaOss() {
+        return "kontakta-oss";
+    }
+
+    // Redirect /hem to /
+    @RequestMapping("/hem")
+    public String redirectToIndex() {
+        return "redirect:/";
+    }
+
+
+
+
 }
